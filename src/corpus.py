ab00fcf02cb536edffdb50613210eca5207d6a43
hamzeh <Hamzeh>
2014-03-11 08:02:49 -0400

    Implemented document loading and corpus initialization with inital feature vector calculations

--- src/corpus.py
+++ src/corpus.py
@@ -4,24 +4,46 @@ from document import document
 import os
 from os import listdir
 from os.path import isfile, join, isdir
+import scipy as sp
 
 class corpus:
+
+	# Flag to allow "Neuron" and "neuron" to be distinguished as separate words
+	allow_case_sensetivity = True
+
+	# Flag to allow words that cannot be identified in a webbased dictionary
+	allow_non_dictionary_words = True
+
+	# Log file to append diagnostic output to
+	log_file = None
+
 	# Dictionary of document objects that this corpus contains (ID,document) pairs
 	documents = {}
 
+	# List of documents sorted by date
+	documents_sorted = []
+
 	# Set of unqiue english words in this corpus
-	words = set([])
+	word_set = set([])
+
+	# List of unqiue english words in this corpus
+	words = []
+
 
 	def add_document(self,file_path):
 		# Initializes this file as a new document in the corpus
 		
-		# load the data
-		f = open(join(file_path), 'r')
-		raw_data = f.read()
-		f.close()
+		try:
+			# load the data
+			f = open(join(file_path), 'r')
+			raw_data = f.read()
+			f.close()
 
-		# create the document object
-		self.documents[file_path] = document(self,raw_data,file_path.split("/")[-1])
+			# create the document object
+			d = document(self,raw_data,file_path.split("/")[-1])
+			self.documents[d.ID] = d
+		except (UnicodeDecodeError):
+			self.log_file.write("UnicodeDecodeError on file " + file_path + "\n")
 
 	def load_documents(self,corp_dir):
 		# Loads all files in dir as documents
@@ -31,16 +53,32 @@ class corpus:
 				self.add_document(f)
 
 	def __init__(self,corp_dir):
+		from interface import log_file_path
+		self.log_file = open(log_file_path,'a')
+
+		#Load all documents
 		self.load_documents(corp_dir)
-		print(self.words)
 
-	def add_words(self,word_set):
+		# Sort documents by date
+		self.documents_sorted = list(self.documents.items())
+		self.documents_sorted.sort(key=lambda x: x[1].published)
+
+		# Make a word list for this corpus
+		self.words = sorted(list(self.word_set))
+
+		# Construct document relational attributes
+		for d in self.documents.values():
+			d.construct_relational_fields()
+			self.log_file.write(str(d.TF) + d.ID + "\n")
+			self.log_file.write(str(d.estimator) + "\n")
+
+		self.log_file.write(str(self.words) + "\n")
+
+		self.log_file.close()
+
+	def add_words(self,w_set):
 		# add word set to words
-		self.words = self.words | word_set
-
-	def get_documents_post_date(self,doc):
-		# Returns the set of documents that were published after doc
-		pass
+		self.word_set = self.word_set | w_set
 
 	def set_citations(self):
 		# sets the citaton data in every document in the corpus
@@ -48,4 +86,41 @@ class corpus:
 
 	def set_influence(self):
 		# sets the influence data in every document in the corpus 
+		pass
+
+	def get_documents_pre_date(self,new_ID):
+		# Returns the set of documents that were published before new
+		new = self.documents[new_ID]
+		if new in self.documents_sorted:
+			return set([d.ID for d in self.documents_sorted[:self.documents_sorted.index(new)]])
+		else:
+			return set([])
+
+	def get_k_nearest_docuemnts(self,k,sim_func,new_ID, document_ID_set):
+		# Finds the k nearest neigbors of the document new within document_set using
+		# sim_func as the simiality measure
+		sims = [] 
+		for d_ID in document_set: 
+			sims.append( (sim_func(self.documents[new_ID].TF,self.documents[d_ID].TF),d_ID) )
+		sims.sort(key = lambda x: x[0])
+		nbr_IDs = []
+		for i in range(k):
+			if(k > len(sims)):
+				break
+			nbr_IDs.append(sims[k][1])
+		return nbr_IDs
+
+	def find_influences(self,new_ID):
+		# Finds all influences of the document new from within this corpus
+		influences = set([])
+		docs_predate_IDs = get_documents_pre_date(new_ID)
+		candidate_IDs = get_k_nearest_docuemnts(100,sp.spatial.distance.cosine,docs_predate_IDs)
+		for c_id in candidate_IDs:
+			if influence_test(c_id,new_ID):
+				influences.add(documents[c_id].ID)
+		return influences
+
+	def influence_test(self,candidate_ID, new_ID):
+		# Tests if candidate is an influence on the document new
+		result = False
 		pass
--- src/document.py
+++ src/document.py
@@ -1,11 +1,25 @@
 #Author: Hamzeh Alsalhi December 2013
 # Collects all necessary operations with significance on the data of a single document in the corpus
 import re
+from datetime import datetime, date, time
+import numpy as np
 
 class document:
 
+	# Log file to append diagnostic output to
+	log_file = None
+
+	# File name
+	file_name = None
+
 	# Unique ID in the corpus
 	ID = None
+
+	# Publication date
+	published = None
+
+	# Document title
+	title = None
 
 	# The corpus object that this document exists in
 	corpus = None
@@ -16,6 +30,9 @@ class document:
 	# The raw data stripped down to only english words, no special syntax included
 	english = None
 
+	# Numpy array of the raw data stripped down to only english words, no special syntax included
+	np_english = None
+
 	# Set of all english words in this documents
 	words = None
 
@@ -23,30 +40,60 @@ class document:
 		# return the english word list representation of raw
 		return [w for w in raw.split() if re.match('^[a-zA-Z0-9\']+$',w) is not None]
 
+	def parse_ID(self,s):
+		# Parses the line that contains the ID for a clean ID
+		return s.split()[0]
+
+	def parse_date(self,s):
+		# Parses the string that represents the date into a date object
+		year_str = str(s[:2])
+		month_str = str(s[2:4])
+		date = datetime.strptime("01"+month_str+year_str,"%d%m%y")
+		return date
+
+	def load_meta_information(self,raw):
+		# Loads ID and publication date
+
+		# Loads ID
+		ID_str = re.search('%Paper:(.*)\n',raw[:500])
+		if(not ID_str == None):
+			self.ID = self.parse_ID(ID_str.group(1))
+			# Loads publication date
+			self.published = self.parse_date(self.ID.split("/")[-1][:4])
+		else:
+			self.ID = "NA"
+			self.log_file.write("ID not found for paper " + self.file_name + "\n")
+
 	def __init__(self,corp,raw_data,file_name):
+		from interface import log_file_path
+		self.log_file = open(log_file_path,'a')
+
 		self.corpus = corp
 
 		# process into three bare representaions
 		self.raw = raw_data
 		self.english = self.get_english(self.raw)
+		self.np_english = np.array(self.english)
 		self.words = set(self.english)
+
+		# Fill in meta information
+		self.file_name = file_name
+		self.load_meta_information(self.raw)
 
 		# update the corpus word set
 		self.corpus.add_words(self.words)
 
-		print(file_name)
-		print(len(self.words))
+		self.log_file.close()
 
-	# The term frequency vector for this paper in realtion to its corpus
+
+	# The term frequency vector for this paper in realtion to its corpus as np array
 	TF = None
 
-	# The TFIDF vector for this paper in realtion to its corpus
+	# The TFIDF vector for this paper in realtion to its corpus as np array
 	TFIDF = None
 
-	# Publication date
-	date = None
-
-	title = None
+	# The estimator as defined in shaparenko 07 to be TF/len(english) for this document as np array
+	estimator = None
 
 	# List of documents in the corpus that this document cites
 	citations = None
@@ -59,3 +106,25 @@ class document:
 
 	# List of documents in the corpus that this document has influenced
 	influenced = None
+
+	def construct_TF_vector(self, corpus_word_list, np_eng):
+		# Constructs a vector of length len(corpus_word_list) that counts how many times each 
+		# word in english occurs
+		tf = np.array([0] * len(corpus_word_list))
+		for i in range(len(corpus_word_list)):
+			tf[i] = len(np.where(np_eng == corpus_word_list[i])[0])
+		return tf
+
+	def construct_estimator(self, corpus_word_list, np_eng):
+		# Constructs a vector of length len(corpus_word_list) that counts how many times each 
+		# word in english occurs and divides each element by len(np_eng)
+		tf = np.array([0] * len(corpus_word_list))
+		for i in range(len(corpus_word_list)):
+			tf[i] = len(np.where(np_eng == corpus_word_list[i])[0])
+		return tf/float(len(np_eng))
+
+	def construct_relational_fields(self):
+		# Build the fields of a document that depend on having a completly defined corpus
+		# such as TF, TFIDF, cited_by, and more
+		self.TF = self.construct_TF_vector(self.corpus.words,self.np_english)
+		self.estimator = self.construct_estimator(self.corpus.words,self.np_english)
